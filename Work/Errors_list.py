2605f530cc6c9f5e125a74fbff1ef51fb7f3a284
Andrew Smith <diver.vlz@gmail.com>
2022-01-25 15:44:31 +0300

    updated structure repository

--- Work/Errors_list.py
+++ Work/Errors_list.py
@@ -1,8 +1,8 @@
-my_errors = [
+errors_list = [
     'did not make a report to CDU dispatcher',
     'did not clarify the weather conditions',
     'did not clarify the current mode of a equipment'
 ]
 
-for error in my_errors:
+for error in errors_list:
     print(error)
