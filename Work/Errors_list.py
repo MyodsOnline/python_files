da11081a7bd054506d5d617cd7ef7d786294afef
Andrew Smith <diver.vlz@gmail.com>
2022-01-25 15:43:15 +0300

    updated structure repository

---
+++ Work/Errors_list.py
@@ -0,0 +1,8 @@
+my_errors = [
+    'did not make a report to CDU dispatcher',
+    'did not clarify the weather conditions',
+    'did not clarify the current mode of a equipment'
+]
+
+for error in my_errors:
+    print(error)
