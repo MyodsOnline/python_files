58205a1858a22b5033587acd935105146bccaa0e
Andrew <diver.vlz@gmail.com>
2023-01-13 12:31:37 +0300

    setter

--- JimShapedCoding/helper.py
+++ JimShapedCoding/helper.py
@@ -1,9 +1,15 @@
+from item import Item
+from phone import Phone
 
-class Item:
-    @staticmethod
-    def is_integer():
-        pass
+# Item.instance_from_csv()
+#
+# print(Item.all_items)
 
-    @classmethod
-    def instance_from_something(cls):
-        pass
+item_1 = Item('MyItem', 750)
+print(item_1.name)
+
+item_1.name = 'OtherItem'
+print(item_1.name)
+
+item_1.name = 'OtherMyItem'  # Will raise Exception
+print(item_1.name)
